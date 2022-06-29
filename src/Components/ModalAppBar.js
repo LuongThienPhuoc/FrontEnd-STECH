70ab572d7530416327cea35a4386f49384ab61d2
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2022-06-29 21:24:13 +0700

    hello

---
+++ src/Components/ModalAppBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Style/ModalAppBar.css'
+function ModalAppBar(props) {
+    return (
+        <div className='modal-app-bar'>
+            <p>Chính trị</p>
+            <p>Dân sinh</p>
+            <p>Lao động</p>
+            <p>Chính trị</p>
+            <p>Giao thông</p>
+            <p>Quỹ hi vọng</p>
+        </div>
+    );
+}
+
+export default ModalAppBar;
--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -3,33 +3,40 @@ import { Container } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home';
 import '../Style/NavigationBar.css'
 import MenuIcon from '@mui/icons-material/Menu';
+import ModalAppBar from './ModalAppBar';
 function NavigationBar(props) {
     return (
 
-        <div style={{ borderBottom: '1px solid #e5e5e5', padding: '5px 0', marginTop:'68px' }}>
+        <div style={{ borderBottom: '1px solid #e5e5e5', padding: '5px 0', marginTop: '68px' }}>
             <Container style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} maxWidth={'xl'}>
                 <div className='btn-navigation' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '5px', borderRadius: '50%' }}>
                     <HomeIcon style={{ fontSize: '16px' }}></HomeIcon>
                 </div>
-                <div className='btn-navigation-item'>Thời sự</div>
-                <div className='btn-navigation-item'>Góc nhìn</div>
-                <div className='btn-navigation-item'>Thế giới</div>
-                <div className='btn-navigation-item'>Video</div>
-                <div className='btn-navigation-item'>Podcasts</div>
-                <div className='btn-navigation-item'>Kinh doanh</div>
-                <div className='btn-navigation-item'>Khoa học</div>
-                <div className='btn-navigation-item'>Giải trí</div>
-                <div className='btn-navigation-item'>Thể thao</div>
-                <div className='btn-navigation-item'>Pháp luật</div>
-                <div className='btn-navigation-item'>Giáo dục</div>
-                <div className='btn-navigation-item'>Sức khỏe</div>
-                <div className='btn-navigation-item'>Đời sống</div>
-                <div className='btn-navigation-item'>Ý kiến</div>
-                <div className='btn-navigation-item'>Tâm sự</div>
-                <div className='btn-navigation-item'>Du Lịch</div>
+                <div className='btn-navigation-item'>
+                    Thời sự
+                    <ModalAppBar></ModalAppBar>
+                </div>
+                <div className='btn-navigation-item'>
+                    Góc nhìn <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Thế giới <ModalAppBar></ModalAppBar>
+                </div>
+                <div className='btn-navigation-item'>Video <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Podcasts <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Kinh doanh <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Khoa học <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Giải trí <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Thể thao <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Pháp luật <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Giáo dục <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Sức khỏe <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Đời sống <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Ý kiến <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Tâm sự <ModalAppBar></ModalAppBar></div>
+                <div className='btn-navigation-item'>Du Lịch <ModalAppBar></ModalAppBar></div>
                 <div className='btn-navigation-item btn-all'>
                     <p style={{ paddingRight: '5px' }}>Tất cả</p>
                     <MenuIcon className='icon'></MenuIcon>
+                    <ModalAppBar></ModalAppBar>
                 </div>
 
             </Container>
